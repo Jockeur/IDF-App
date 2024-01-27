62ab00bef87e54944c0ef45325e364af6a7a5f2d
Jockeur <jocceur@gmail.com>
2024-01-27 01:53:09 +0100

    everything's great

--- logic.py
+++ logic.py
@@ -1,8 +1,10 @@
 from time import gmtime
 from math import floor
 from requests import get
-import dateutil
+from dateutil.parser import isoparse
 
+import dateutil
+import time
 import json
 
 class Requester():
@@ -12,18 +14,40 @@ class Requester():
         self.line_id = line_id
 
     def request_next_passage(self):
-        headers = {'apiKey' : 'API-KEY'}
+        headers = {'apiKey': 'API-KEY'}
         response = get(url=f"https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF%3AStopPoint%3AQ%3A{self.stop_point_id}%3A&LineRef=STIF%3ALine%3A%3A{self.line_id}%3A",
                        headers=headers)
         
         self._get_next_passage(response.json())
+        print(response.json())
         
     def _get_next_passage(self, datas):
         with open(self.file, "r+") as file:
-            data = json.load(file)
-            data += datas
             file.seek(0)
-            json.dump(data, file, indent=4)
+            json.dump(datas, file, indent=4)
             file.truncate()
-        
 
+
+class Timer():
+    def __init__(self, file):
+        self.file = file
+        self.current_time = isoparse(self._convertISO(gmtime()))
+        self.bus_time = ''
+        self.delta = None
+        self.difference = 0
+
+    def _convertISO(self, t):
+        return time.strftime("%Y-%m-%dT%H:%M:%S.000Z", t)
+    
+    def get_bus_time(self):
+        with open(self.file, "r+") as file:
+            data = json.load(file)
+            bus_time = data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedArrivalTime"]
+            self.bus_time = isoparse(bus_time)
+
+            print(self.bus_time)
+
+    def get_difference(self):
+        self.delta = self.bus_time - self.current_time
+        self.difference = floor(self.delta.seconds/60)
+        print(self.difference)
