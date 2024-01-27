5e0d9a53a8174f4543aa712f391deec0a7b97e12
Jockeur <jocceur@gmail.com>
2024-01-27 01:03:40 +0100

    oops

--- logic.py
+++ logic.py
@@ -5,7 +5,6 @@ import dateutil
 
 import json
 
-
 class Requester():
     def __init__(self, file, stop_point_id, line_id):
         self.file = file
@@ -13,7 +12,7 @@ class Requester():
         self.line_id = line_id
 
     def request_next_passage(self):
-        headers = {'apiKey' : 'tlsWi88p42EWWPuizf5epIlG20BVpwKl'}
+        headers = {'apiKey' : 'API-KEY'}
         response = get(url=f"https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF%3AStopPoint%3AQ%3A{self.stop_point_id}%3A&LineRef=STIF%3ALine%3A%3A{self.line_id}%3A",
                        headers=headers)
         
@@ -27,3 +26,4 @@ class Requester():
             json.dump(data, file, indent=4)
             file.truncate()
         
+
